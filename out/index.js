fb08f6af3470b03f7f3e557d777e7a65a2e35107
omelhoro <igor.fischer154@gmail.com>
2016-06-04 18:58:09 +0200

    added more files to ignored

--- out/index.js
+++
@@ -1,5 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.string.StringBuffer', 'goog.array']);
-goog.addDependency("../C0A7D7A.js", ['cljs.nodejs'], ['cljs.core']);
-goog.addDependency("../russyll/server.js", ['russyll.server'], ['cljs.core', 'cljs.nodejs']);
-goog.addDependency("../BCE03FE.js", ['cljs.nodejscli'], ['cljs.core', 'cljs.nodejs']);
